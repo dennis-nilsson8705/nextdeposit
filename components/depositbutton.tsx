b471efeb509602f74107769b89c3c6f8c1afe339
dennis-nilsson8705 <dennis.nilsson8705@gmail.com>
2023-01-11 17:03:06 +1300

    dispatch

--- components/depositbutton.tsx
+++ components/depositbutton.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setTotalDepositAmount } from '../redux/depositSlice';
 import store from '../redux/store';
 
 const DepositButton = () => {
   const [title, setTitle] = useState('Submit');
+  const dispatch = useDispatch();
   function handleClick() {
     const currentState = store.getState();
-    console.log(currentState);
-
+    const addTodo = dispatch(setTotalDepositAmount(3));
     title === 'Submit' && setTitle('Success');
   }
   return (
     <button
       className="pt-4 pb-4 pl-4 pr-4 font-bold text-white bg-gray-600 rounded hover:bg-blue-700"
       onClick={handleClick}
-      disabled={title === 'Success'}
+      // disabled={title === 'Success'}
     >
       {title}
     </button>
